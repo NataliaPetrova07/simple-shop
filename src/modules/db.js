3dcbc1df523184de8189bc9972b563540f1db4aa
natalia <nata774e@stud.kea.dk>
2022-11-22 18:41:23 +0100

    done-ish

--- src/modules/db.js
+++ src/modules/db.js
@@ -1,7 +1,7 @@
-export function insertOrder(payload) {
+export async function insertOrder(payload) {
   const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhbmNrbXR2eGpqcmtkeWJidGp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY1OTg3NTUsImV4cCI6MTk4MjE3NDc1NX0.lh2QqSjJdb1zNk6ry89TNdIBHckN-Lg6Kbdn5JnGXnk";
   const url = "https://fanckmtvxjjrkdybbtjv.supabase.co";
-  fetch(url + "/rest/v1/simpleshop", {
+  const res = await fetch(url + "/rest/v1/simpleshop", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -10,7 +10,6 @@ export function insertOrder(payload) {
       Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhbmNrbXR2eGpqcmtkeWJidGp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY1OTg3NTUsImV4cCI6MTk4MjE3NDc1NX0.lh2QqSjJdb1zNk6ry89TNdIBHckN-Lg6Kbdn5JnGXnk",
     },
     body: JSON.stringify(payload),
-  })
-    .then()
-    .then();
+  });
+  return await res.json();
 }
